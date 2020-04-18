43b257f7387d4155b5284ca430caf2ce0549ca7a
Brenda Bojórquez <61170163+brendabojorquez8@users.noreply.github.com>
2020-04-18 00:49:05 -0600

    Initial commit

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import "./footer.css";
+
+class Footer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <img src="https://i.imgur.com/z7MV9az.jpg" alt="" className="footer" />
+      </div>
+    );
+  }
+}
+
+export default Footer;
